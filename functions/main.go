d7aae10c11fd5323b028e000acf7b218d0c4caa9
brunocoronado49 <francorangelcoronado@gmail.com>
2025-07-06 03:30:52 -0600

    🍻 Finish simple functions file

--- functions/main.go
+++ functions/main.go
@@ -19,6 +19,16 @@ func plusPlus(a, b, c int) int {
 	return a + b + c
 }
 
+// Go has built-in support for multiple return values. this feature
+// is used often in idiomatic Go, for example to return both result and
+// error values from a function
+
+// The (int, int) in this function signature shows that the function
+// return 2 ints
+func vals() (int, int) {
+	return 3, 7
+}
+
 // Call a function just as you'd expect, with name(args)
 func main() {
 	res := plus(1, 2)
@@ -26,4 +36,15 @@ func main() {
 
 	res = plusPlus(1, 2, 3)
 	fmt.Println("1+2+3 =", res)
+
+	// Here we use the 2 different return values from the call
+	// with multiple assignment
+	a, b := vals()
+	fmt.Println(a)
+	fmt.Println(b)
+
+	// If you only want a subset of the returned values, use
+	// the blank identifier "_"
+	_, c := vals()
+	fmt.Println(c)
 }
