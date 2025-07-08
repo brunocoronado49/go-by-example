9ec193a474031ac1b4b9f926b9483e5f48b7b7bf
brunocoronado49 <francorangelcoronado@gmail.com>
2025-07-08 11:00:31 -0600

    Finish the builtin type file

--- range-over-builtin-types/main.go
+++ range-over-builtin-types/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // Range iterates over elements in a variety of built-in data
 // structures. Let's see how to use range with some of the data
 // structures we've already learned
@@ -10,5 +12,32 @@ func main() {
 	sum := 0
 	for _, num := range nums {
 		sum += num
+	}
+	fmt.Println("sum:", sum)
+
+	// range on arrays and slices provides both the index and value for
+	// each entry. Above we didn't need the index, so we ignored it with the blank
+	// identifier "_". Sometimes we actually want the indexes though
+	for i, num := range nums {
+		if num == 3 {
+			fmt.Println("index:", i)
+		}
+	}
+
+	// range on map iterates over key/value pairs
+	kvs := map[string]string{"a": "apple", "b": "banana"}
+	for k, v := range kvs {
+		fmt.Printf("%s -> %s\n", k, v)
+	}
+
+	// range can also iterate over just keys of a map
+	for k := range kvs {
+		fmt.Println("key:", k)
+	}
+
+	// range on strings iterates over Unicode code points. The first value
+	// is the starting byte index of the rune and the second the rune itself.
+	for i, c := range "go" {
+		fmt.Println(i, c)
 	}
 }
